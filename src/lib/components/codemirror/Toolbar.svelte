59e528c95468453bec1ad4fd99d9507044af5e2c
Eli <88557639+lishaduck@users.noreply.github.com>
2025-11-24 11:10:47 -0600

    feat: slightly prettier toolbar

--- src/lib/components/codemirror/Toolbar.svelte
+++ src/lib/components/codemirror/Toolbar.svelte
@@ -18,10 +18,10 @@
 
 <div class="flex items-center gap-2 border-b border-base-content/10 px-4 py-2">
   {#each tools as toolset, index (index)}
-    <div class="flex items-center gap-0.5 rounded-lg bg-base-300 p-1 shadow">
+    <div class="flex items-center gap-0.5 rounded-lg p-1">
       {#each toolset as tool (tool.title)}
         {@const Icon = tool.icon}
-        <button onclick={tool.onclick} class="btn shadow" title={tool.title}>
+        <button onclick={tool.onclick} class="btn" title={tool.title}>
           <Icon size={18} />
         </button>
       {/each}

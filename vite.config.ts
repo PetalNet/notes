6a36a7c602045ed8c197e1c844168710a664f134
Eli <88557639+lishaduck@users.noreply.github.com>
2025-11-26 12:59:11 -0600

    fix: implicit any
    Should probably upstream it at some point but just a quick fix for now

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import devtoolsJson from "vite-plugin-devtools-json";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 
 import wasm from "vite-plugin-wasm";
@@ -8,7 +8,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
   plugins: [
-    wasm(),
+    wasm() as Plugin,
     topLevelAwait(),
     tailwindcss(),
     sveltekit(),

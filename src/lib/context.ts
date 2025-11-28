bc60039e94f5b80956f6b9c5a1b5598dc9d3b6f7
Eli <88557639+lishaduck@users.noreply.github.com>
2025-11-27 20:00:03 -0600

    chore: delete unused LoroNoteManager context

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,6 +1,3 @@
-import type { LoroNoteManager } from "$lib/loro";
 import { createContext } from "svelte";
 
 export const [getUserPrivateKey, setUserPrivateKey] = createContext<string>();
-export const [getLoroManager, setLoroManager] =
-  createContext<LoroNoteManager>();

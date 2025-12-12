76240757c3d8699657786e16cdbe213dcf05de47
Eli <88557639+lishaduck@users.noreply.github.com>
2025-12-12 14:40:26 -0600

    refactor: less unawaited

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -302,19 +302,18 @@
     {#if contextMenu.isFolder}
       <button
         class="flex w-full cursor-pointer items-center gap-2 px-4 py-2 text-left text-sm text-base-content hover:bg-primary hover:text-primary-content"
-        onclick={() => {
+        onclick={async () => {
           if (user === undefined) {
             throw new Error("Cannot create note whilst logged out.");
           }
 
-          unawaited(
-            handleCreateNote(
-              "An Untitled Note",
-              clickedId,
-              false,
-              user.publicKey,
-            ),
+          await handleCreateNote(
+            "An Untitled Note",
+            clickedId,
+            false,
+            user.publicKey,
           );
+
           closeContextMenu();
         }}><Plus /> New Note Inside</button
       >
